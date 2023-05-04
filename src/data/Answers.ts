de487da594eec82252bae206b8e7fc850c3a283f
Eric Sheen <eric.sprog@gmail.com>
2023-05-04 11:38:32 -0400

    grammar

--- src/data/Answers.ts
+++ src/data/Answers.ts
@@ -19,27 +19,27 @@ export const Answers: AnswerModel[] = [
     },
     {
         question: "What is an introductory call?",
-        answer: "An interview process typically starts with an 'Introductory call' where the recruiter tries to get to know you better! They may ask behavioural questions and about your past work experience. Practise explaining relavent information on your resume and behavioural questions that the interviewer may ask you. Also practise your literal introduction of who you are! I found it most helpful to practise with a friend in-person or on an online meeting. It better prepares you rather than practising by yourself.",
+        answer: "An interview process typically starts with an 'Introductory call' where the recruiter tries to get to know you better! They may ask behavioural questions and questions about your past work experience. Practise explaining relevant information on your resume and behavioural questions that the interviewer may ask you. Also, practise your literal introduction of who you are! I found it most helpful to practise with a friend in-person or in an online meeting. It better prepares you rather than practising by yourself.",
         link: ["https://ca.indeed.com/career-advice/interviewing/screening-interview-questions", "https://enhancv.com/blog/internship-interview-questions-and-answers/"]
     },
     {
         question: "What to expect in a technical internview?",
-        answer: "Once you have passed the intro call you will most likely have a technical assessment for the company to decide whether or not you are fit for the role. This may be in a leetcode-style, takehome project, or peer programming based format. You will most likely be told what to expect and if not, you should probably ask. If it's a takehome project, you will be given a deadline and project to complete. If it's a leetcode-style, then it will be a live interview setting where you will try and solve a question that involves algorithms. If it's peer programming then this can vary. In my experience, peer programming interviews were a live format focussing on debugging and understanding a codebase. There can be multiple technical rounds/interviews.",
+        answer: "Once you have passed the intro call you will most likely have a technical assessment for the company to decide whether or not you are fit for the role. This may be in a leetcode-style, takehome project, or peer programming-based format. You will most likely be told what to expect and if not, you should probably ask. If it's a take-home project, you will be given a deadline and project to complete. If it's a leetcode-style, then it will be a live interview setting where you will try and solve a question that involves algorithms. If it's peer programming then this can vary. In my experience, peer programming interviews were a live format focussing on debugging and understanding a codebase. There can be multiple technical rounds/interviews.",
         link: ["https://leetcode.com/discuss/career/450215/How-to-use-LeetCode-to-help-yourself-efficiently-and-effectively-(for-beginners)", "https://snaphunt.com/advice/jobSearchAdvice/seven-tips-to-prepare-for-leetcode-style-questions-in-technical-interviews", "https://www.thisdot.co/blog/how-to-avoid-common-pitfalls-and-ace-your-take-home-assignment", "https://www.freecodecamp.org/news/things-ive-learned-from-pair-programming-interviews-35a4db7d7443/"]
     },
     {
         question: "How can I succeed in technical interview?",
-        answer: "Practise & study. Do not leave preperations to the last minute no matter how experienced or confident you are! During the interview be sure to COMMUNICATE. I remember having an interview with Shopify during high school and one point of feedback was to communicate! You should be actively communicating to your interviewer about what your doing. Do not stay silent (like I did kinda) and just code away. The technical interviewer is looking for someone that can articulate their thoughts and thinking process clearly.",
+        answer: "Practise & study. Do not leave preparations to the last minute no matter how experienced or confident you are! During the interview be sure to COMMUNICATE. I remember having an interview with Shopify during high school and one point of feedback was to communicate! You should be actively communicating with your interviewer about what you're doing. Do not stay silent and just code away. The technical interviewer is looking for someone that can articulate their thoughts and thinking process clearly.",
         link: []
     },
     {
         question: "What happens afterwards?",
-        answer: "This may vary. In my experience I had another meeting with the recruiter to talk about what I'm going to work on, the team, and any questions I have (so come prepared with questions you may have). There may also be something called a 'reference check,' where the interviewer will ask you for a reference from a previous employer or person. This is just to make sure that you weren't an asshole in your previous internship.",
+        answer: "This may vary. In my experience, I had another meeting with the recruiter to talk about what I'm going to work on, the team, and any questions I have (so come prepared with questions you may have). There may also be something called a 'reference check,' where the interviewer will ask you for a reference from a previous employer or person. This is just to make sure that you weren't an asshole in your previous internship.",
         link: []
     },
     {
         question: "I got my offer! Should I negotiate?",
-        answer: "Depends. If the company has hard set internship rates then negotiating will probably be pointless because they have a budget. If you have the leverage (like other higher offers), you can definetly negotiate because you have something to lean back on. However, if you have no leverage than it's a little risky. I suggest you think it over and practise how you are going to go about explaning it. One thing I don't recommend is asking for a value WAY higher than the offered because that can scare away the recruiter and they'll reject you. In my experience, I was a returning intern and negotiated my offer higher because this was a return offer. I already knew how the company operated and had a positive prior experience with them.",
+        answer: "Depends. If the company has hard-set internship rates then negotiating will probably be pointless because they have a budget. If you have the leverage (like other higher offers), you can definitely negotiate because you have something to lean back on. However, if you have no leverage then it's a little risky. I suggest you think it over and practise how you are going to go about explaining it. One thing I don't recommend is asking for a value WAY higher than the one offered because that can scare away the recruiter and they'll reject you. In my experience, I was a returning intern and negotiated my offer higher because this was a return offer. I already knew how the company operated and had a positive prior experience with them.",
         link: ["https://github.com/codepath/student-career-handbook/blob/master/offer-and-salary-negotiation/negotiating-on-internship-salary.md"]
     },
     {
@@ -49,7 +49,7 @@ export const Answers: AnswerModel[] = [
     },
     {
         question: "I have imposter syndrome. :(",
-        answer: "When I started off I definetly felt like I wasn't the most qualified but who cares! We're here to learn. I feel like everyone has experienced imposter syndrome to a certain degree and you're not alone. It's ok to feel this way but don't let it demotivate you or undervalue you. Stay confident and continue to work on your craft! Your mindset determines your success.",
+        answer: "When I started, I definitely felt like I wasn't the most qualified but who cares! We're here to learn. I feel like everyone has experienced imposter syndrome to a certain degree and you're not alone. It's ok to feel this way but don't let it demotivate you or undervalue you. Stay confident and continue to work on your craft! Your mindset determines your success.",
         link: [] 
     },
     
